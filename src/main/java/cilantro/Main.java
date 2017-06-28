6d2f71d4bcbd9380694ee32d4f57ad4af0988f41
Aaron Brayall <abrayall@yahoo.com>
2017-06-28 09:27:33 -0400

    updating Main with print helper methods

--- src/main/java/cilantro/Main.java
+++ src/main/java/cilantro/Main.java
@@ -1,5 +1,7 @@
 package cilantro;
 
+import java.io.PrintStream;
+
 import javax.lang.System;
 import javax.util.List;
 import javax.util.Map;
@@ -21,6 +23,24 @@ public class Main {
 		this.parameters = parser.parameters(arguments);
 		this.options = parser.options(arguments);
 		this.console = new Console(System.out, System.err, System.in).install();
+		return this;
+	}
+	
+	public Main print(String message, Object... parameters) {
+		return print(console.out, message, parameters);
+	}
+	
+	public Main print(PrintStream stream, String message, Object... parameters) {
+		console.printf(stream, message, parameters);
+		return this;
+	}
+	
+	public Main println(String message, Object... parameters) {
+		return println(console.out, message, parameters);
+	}
+	
+	public Main println(PrintStream stream, String message, Object... parameters) {
+		console.printlnf(stream, message, parameters);
 		return this;
 	}
 	
