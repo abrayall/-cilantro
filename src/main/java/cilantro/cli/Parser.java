383eb9230a6c3342c9db96a4257c0bfcd74ff12a
Aaron Brayall <abrayall@yahoo.com>
2017-07-01 10:07:08 -0400

    updating cli parser API to be a bit more friendly

--- src/main/java/cilantro/cli/Parser.java
+++ src/main/java/cilantro/cli/Parser.java
@@ -8,8 +8,26 @@ import static javax.util.List.*;
 
 public class Parser {
 	
+	protected String[] arguments;
+	
+	public Parser() {
+		this(new String[0]);
+	}
+	
+	public Parser(String[] arguments) {
+		this.arguments = arguments;
+	}
+	
+	public Arguments parse() {
+		return parse(this.arguments);
+	}
+	
 	public Arguments parse(String[] arguments) {
 		return new Arguments(arguments, parameters(arguments), options(arguments));
+	}
+	
+	public List<String> parameters() {
+		return parameters(this.arguments);
 	}
 	
 	public List<String> parameters(String[] arguments) {
@@ -22,6 +40,10 @@ public class Parser {
 		return parameters;
 	}
 
+	public Map<String, String> options() {
+		return options(this.arguments);
+	}
+	
 	public Map<String, String> options(String[] arguments) {
 		Map<String, String> options = map();
 		for (String argument : arguments) {
