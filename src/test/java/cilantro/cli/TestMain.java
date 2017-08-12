d2eeb9aede5696da505d6de6f06a8ab3f64cd1d0
Aaron Brayall <abrayall@yahoo.com>
2017-08-11 21:00:32 -0400

    updating unit tests for Main to cover argument parsing

--- src/test/java/cilantro/cli/TestMain.java
+++ src/test/java/cilantro/cli/TestMain.java
@@ -1,5 +1,33 @@
 package cilantro.cli;
 
-public class TestMain extends cilantro.Main {
+import javax.lang.Assert;
 
+import cilantro.Main;
+
+public class TestMain {
+
+	public void test() throws Exception {
+		Main.main(Test.class, new String[] {"foo", "bar", "--test", "-test2=test2", "foo2"});
+		Assert.equals(3, Test.ARGUMENTS.parameters.size());
+		Assert.equals("foo", Test.ARGUMENTS.parameters.get(0));
+		Assert.equals("bar", Test.ARGUMENTS.parameters.get(1));
+		Assert.equals("foo2", Test.ARGUMENTS.parameters.get(2));
+		
+		Assert.equals(2, Test.ARGUMENTS.options.size());
+		Assert.equals("true", Test.ARGUMENTS.options.get("test"));
+		Assert.equals("test2", Test.ARGUMENTS.options.get("test2"));
+	}
+	
+	public static void main(String[] arguments) throws Exception {
+		new TestMain().test();
+	}
+		
+	public static class Test extends cilantro.Main {
+		public static Arguments ARGUMENTS;
+		
+		public Integer execute(Arguments arguments) {
+			ARGUMENTS = arguments;
+			return null;
+		}
+	}
 }
